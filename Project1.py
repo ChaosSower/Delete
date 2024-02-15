0853b6d5729efe30e2821bf849fef956093ed287
ChaosSower <evtykhin2003@gmail.com>
2024-02-15 14:33:56 +0300

    Help git

---
+++ Project1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window = Tk()
+window.title("Добро пожаловать в приложение PythonRu")
+window.geometry('400x250')
+lbl = Label(window, text="Привет", font=("Arial Bold", 50), fg="Red")
+lbl.grid(column=0, row=0)
+btn = Button(window, text="Не нажимать!")
+btn.grid(column=1, row=0)
+window.mainloop()
